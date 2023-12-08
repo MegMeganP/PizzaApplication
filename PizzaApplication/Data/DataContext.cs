8588b8a11b38f1a1c8bf2cbc1ab37b3bd02a8b34
MegMeganP <80060671+MegMeganP@users.noreply.github.com>
2023-12-08 09:49:22 -0700

    Add project files.

---
+++ PizzaApplication/Data/DataContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaApplication.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public string ConnectionString { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (ConnectionString == null)
+            {
+                ConnectionString = "Data Source=megansqlserver2022.database.windows.net,1433;Initial Catalog=PizzaApplication;Integrated Security=False;User ID=MeganSqlServer;Password=M3ganSqlServer;Encrypt=True;TrustServerCertificate=False;";
+            }
+
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Topping>().HasIndex(p => p.ToppingName).IsUnique();
+            modelBuilder.Entity<Pizza>().HasIndex(p => p.Name).IsUnique();
+        }
+
+
+
+    }
+}
